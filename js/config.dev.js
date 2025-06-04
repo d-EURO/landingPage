0d1b288ac6a5f99eae2a3bd1b2812453da272e85
hermann-eske <hermann.eske@gmail.com>
2025-06-04 21:19:01 +0200

    Chain.report als Partner hinzugefügt

--- js/config.dev.js
+++
@@ -1,4 +0,0 @@
-window.ENV_CONFIG = {
-  DFX_API_URL: "https://dev.api.dfx.swiss",
-  DEURO_API_URL: "https://dev.api.deuro.com"
-};
--- js/config.prod.js
+++
@@ -1,4 +0,0 @@
-window.ENV_CONFIG = {
-  DFX_API_URL: "https://api.dfx.swiss",
-  DEURO_API_URL: "https://api.deuro.com"
-};

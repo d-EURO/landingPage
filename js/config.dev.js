4cd4ebf3ef7c1bd7a0330e1baeaf174cee3eb371
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2025-06-05 13:31:34 +0200

    Revert "Merge pull request #50 from d-EURO/develop"
    This reverts commit 599f17cf579939869dcfcf1970a6ba9091179561, reversing
    changes made to ef56db0d75128459f7f0199c1539f03b1d158a17.

---
+++ js/config.dev.js
@@ -0,0 +1,4 @@
+window.ENV_CONFIG = {
+  DFX_API_URL: "https://dev.api.dfx.swiss",
+  DEURO_API_URL: "https://dev.api.deuro.com"
+};
---
+++ js/config.js
@@ -0,0 +1,6 @@
+const config = {
+  dfxApiBaseUrl: import.meta.env.VITE_DFX_API_URL,
+  deuroApiBaseUrl: import.meta.env.VITE_DEURO_API_URL,
+};
+
+export default config;
